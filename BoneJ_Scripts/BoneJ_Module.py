94288d4ea286794dd8c159a2e26cc8d8c846c4e0
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-01-17 15:14:45 -0500

    Update BoneJ_Module.py

--- BoneJ_Scripts/BoneJ_Module.py
+++ BoneJ_Scripts/BoneJ_Module.py
@@ -300,12 +300,12 @@ def Ellipsoid_Factor(array,voxel_size,fiji_path,nVectors = 100,vectorIncrement =
         print(metric_dict)
 
         
-# Thickness_result = Thickness(array,voxel_size,fiji_path,showMaps = True, maskArtefacts = True)
-# Spacing_result = Spacing(array,voxel_size,fiji_path,showMaps = True, maskArtefacts = True)
-# Area_VolumeFraction_result = Area_VolumeFraction(array,voxel_size,fiji_path)
-# Connectivity_result = Connectivity(array,voxel_size,fiji_path)
-# Anisotropy_result = Anisotropy(array,voxel_size,fiji_path,NDirs = 2000, nLines = 10000, samplingincrement = 1.73, 
-# radii = False, eigens = False)
+Thickness_result = Thickness(array,voxel_size,fiji_path,showMaps = True, maskArtefacts = True)
+Spacing_result = Spacing(array,voxel_size,fiji_path,showMaps = True, maskArtefacts = True)
+Area_VolumeFraction_result = Area_VolumeFraction(array,voxel_size,fiji_path)
+Connectivity_result = Connectivity(array,voxel_size,fiji_path)
+Anisotropy_result = Anisotropy(array,voxel_size,fiji_path,NDirs = 2000, nLines = 10000, samplingincrement = 1.73, 
+radii = False, eigens = False)
 Ellipsoid_Factor(array, voxel_size, fiji_path,nVectors = 100,vectorIncrement =.435,skipRatio =1,contactSensitivity = 1
 ,maxIterations = 100,maxDrift = .4,runs = 1,seedOnDistanceRidge = True,distanceThreshold = .6,seedOnTopologyPreserving = True
 ,showFlinnPlots = True,showConvergence = True,showSecondaryImages = True)
