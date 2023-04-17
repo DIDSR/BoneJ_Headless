f8b21787457307aa1ac21d95ad94da399604cdd0
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-04-17 14:17:46 -0400

    Update Trabecular_Thickness_API.py

--- BoneJ_Scripts/Trabecular_Thickness_API.py
+++ BoneJ_Scripts/Trabecular_Thickness_API.py
@@ -29,7 +29,6 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 #Open image from path 
-# input_dir = "/gpfs_projects/sriharsha.marupudi/Segmentations_Otsu_Print_100/"
 outputdir 
 
 #open input image as IJ1-style image to be compatabile with wrapper  
