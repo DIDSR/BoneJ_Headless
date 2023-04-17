691435e0da626d4684dbb2e63d63fe44d257e217
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-04-17 11:55:38 -0400

    Update BoneJ_Metrics_Script.py

--- BoneJ_Scripts/BoneJ_Metrics_Script.py
+++ BoneJ_Scripts/BoneJ_Metrics_Script.py
@@ -7,30 +7,29 @@ import csv
 import tempfile 
 import os
 import subprocess
-import pandas as pd 
 import os 
 from glob import glob 
 
-out_dir1 = "/gpfs_projects/sriharsha.marupudi/Thickness_Measurements_Print_40"
+out_dir1 = "/Thickness_Measurements/"
 os.makedirs(out_dir1, exist_ok=True)
 
-out_dir2 = "/gpfs_projects/sriharsha.marupudi/Spacing_Measurements_Print_40"
+out_dir2 = "/Spacing_Measurements/"
 os.makedirs(out_dir2, exist_ok=True)
 
-out_dir3 = "/gpfs_projects/sriharsha.marupudi/Anisotropy_Measurements_MIL_L1_100"
+out_dir3 = "/Anisotropy_Measurements/"
 os.makedirs(out_dir3, exist_ok=True)
 
-out_dir4 = "/gpfs_projects/sriharsha.marupudi/Area_VolumeFraction_Measurements_Print_40"
+out_dir4 = "/Area_VolumeFraction_Measurements/"
 os.makedirs(out_dir4, exist_ok=True)
 
-out_dir5 = "/gpfs_projects/sriharsha.marupudi/Connectivity_Measurements_Print_40"
+out_dir5 = "/Connectivity_Measurements/"
 os.makedirs(out_dir5, exist_ok=True)
 
- out_dir6 = "/gpfs_projects/sriharsha.marupudi/Ellipsoid_Factor_Measurements_Print_40"
+ out_dir6 = "/Ellipsoid_Factor/"
  os.makedirs(out_dir6, exist_ok=True)
 
 
-ROIDir = "/gpfs_projects/sriharsha.marupudi/Segmentations_Otsu_Print_40/"
+ROIDir = "/BoneJ_Headless/ROIs"
 
 
 
@@ -38,10 +37,7 @@ ROINRRD = glob(ROIDir+"*.nrrd")
 
 
 for txt in ROINRRD:
-    NAME = os.path.basename(txt).replace("Segmentation-grayscale-Print-","").replace(".nrrd","")
-    print (f"output ROI-{NAME}")
-
-            
+    NAME = os.path.basename(txt).replace(".nrrd","")
 
 import BoneJ_Demo_Script_Area_VolumeFraction
 import BoneJ_Demo_Script_Thickness
