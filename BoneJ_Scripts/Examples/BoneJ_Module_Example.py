f32cb91ca84ad85c5330ba8e29aee0a34e5f42c3
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-04-17 11:41:28 -0400

    Update BoneJ_Module_Example.py

--- BoneJ_Scripts/Examples/BoneJ_Module_Example.py
+++ BoneJ_Scripts/Examples/BoneJ_Module_Example.py
@@ -18,13 +18,9 @@ import sys, os
 # Define function for each individual plugin 
 #Require installation of Fiji with BoneJ plugins
 
-NAME = "1_06604"
-# array,array1header = nrrd.read(f"/gpfs_projects/sriharsha.marupudi/Segmentations_Otsu_L1/Segmentation-grayscale-{NAME}.nrrd")  # should be a numpy array
-array,array1header = nrrd.read(f"/gpfs_projects/sriharsha.marupudi/Segmentations_Otsu_Print/Segmentation-grayscale-Print-{NAME}.nrrd")
+array,array1header = nrrd.read(volume) #Should be numpy array
 voxel_size = [51.29980, 51.29980, 51.29980] #microns 
 fiji_path = "~/Fiji.app/ImageJ-linux64"
-
-
 
 # feed in numpy array
 
