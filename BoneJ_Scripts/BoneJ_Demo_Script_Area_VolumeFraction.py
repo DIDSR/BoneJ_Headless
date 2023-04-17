8e74b81671d62185eea1e4b600b6c6cbc960ffc3
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-04-17 13:56:12 -0400

    Update BoneJ_Demo_Script_Area_VolumeFraction.py

--- BoneJ_Scripts/BoneJ_Demo_Script_Area_VolumeFraction.py
+++ BoneJ_Scripts/BoneJ_Demo_Script_Area_VolumeFraction.py
@@ -9,27 +9,21 @@ import os
 import subprocess 
 from glob import glob
 
-ROIDir = "/gpfs_projects/sriharsha.marupudi/Segmentations_Otsu_Print_25/"
-ROINRRD = glob(ROIDir+"Segmentation-grayscale-Print-*.nrrd")
+ROIDir = "/BoneJ_Headless_ROIs/"
+ROINRRD = glob(ROIDir+"*.nrrd")
 
 
 
 for txt in ROINRRD:
     
-    NAME = os.path.basename(txt).replace("Segmentation-grayscale-Print-","").replace(".nrrd","")
+    NAME = os.path.basename(txt).replace(".nrrd","")
     print(f"output ROI-{NAME}.")
-   
-    
-    # seg, header = nrrd.read(f"{ROIDir}/ROI-{NAME}.nrrd")
-    tempdir = "/gpfs_projects/sriharsha.marupudi/Area_VolumeFraction_Measurements_Print_25/"
+    tempdir = "/...Measurements/"
     data1_nrrd = os.path.join(tempdir,"img.nrrd")
     table_csv = os.path.join(tempdir, "table.csv")
     outputdir = os.path.join(tempdir)
-
     
-    # TODO: from {file with your BoneJ wrapper} import compute_bonej_thickness
-    
-    data1,data1header1 = nrrd.read(ROIDir+f"Segmentation-grayscale-Print-{NAME}.nrrd")
+    data1,data1header1 = nrrd.read(ROIDir+f"{NAME}.nrrd")
     ### save data1 to temporaryDirectory
     header = {'units': ['um', 'um', 'um'],'spacings': [51.29980,51.29980,51.29980]}
     nrrd.write(data1_nrrd,data1,header)
@@ -37,7 +31,7 @@ for txt in ROINRRD:
     
     # TODO: run your BoneJ thickness wrapper
     # table is the boneJ table, thickness_image is a numpy array containing thickness image
-    macro_file = "/gpfs_projects/sriharsha.marupudi/Area_VolumeFraction_API_.py"
+    macro_file = "/BoneJ_Headless/Area_VolumeFraction_API_.py"
     
     fiji_path = "~/Fiji.app/ImageJ-linux64" # where is this
     
