879f273d8cc044deb6cb8aa351017030512f19a5
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-04-17 14:16:53 -0400

    Update Connectivity_API_Test.py

--- BoneJ_Scripts/Connectivity_API_Test.py
+++ BoneJ_Scripts/Connectivity_API_Test.py
@@ -31,7 +31,6 @@ from ij.io import FileSaver
 
 #Open image from path 
 outputdir
-# outputdir = "/gpfs_projects/sriharsha.marupudi/Measurements_Test/" 
 
 IJ.run("Clear BoneJ results");
 IJ.open(image)
