21ec6b081af80eb19b47250b1038b20d19754c8b
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-04-17 14:10:47 -0400

    Update BoneJ_Demo_Script_Spacing.py

--- BoneJ_Scripts/BoneJ_Demo_Script_Spacing.py
+++ BoneJ_Scripts/BoneJ_Demo_Script_Spacing.py
@@ -16,41 +16,34 @@ from glob import glob
 
 #from file import NAME 
 
-ROIDir = "/gpfs_projects/sriharsha.marupudi/Segmentations_Otsu_Print_25/"
-showMaps ="True"
+ROIDir = "/BoneJ_Headless_ROIs/"
+showMaps = "True"
 maskArtefacts = "True"
 mapChoice = "Trabecular spacing"
 
-ROINRRD = glob(ROIDir+"Segmentation-grayscale-Print-*.nrrd")
+ROINRRD = glob(ROIDir+"*.nrrd")
 
 for txt in ROINRRD:
     
-    NAME = os.path.basename(txt).replace("Segmentation-grayscale-Print-","").replace(".nrrd","")
+    NAME = os.path.basename(txt)..replace(".nrrd","")
 
     print(f"output ROI-{NAME}.")
     
-    # seg, header = nrrd.read(f"{ROIDir}/ROI-{NAME}.nrrd")
-
-    tempdir = "/gpfs_projects/sriharsha.marupudi/Spacing_Measurements_Print_25/"
+    tempdir = "/gpfs_projects/sriharsha.marupudi/...Measurements/"
     data1_nrrd = os.path.join(tempdir,"img.nrrd")
     spacing_tif = os.path.join(tempdir,"spacing.tif")
     table_csv = os.path.join(tempdir,"table.csv")
     outputdir = os.path.join(tempdir)
 
-
-# TODO: from {file with your BoneJ wrapper} import compute_bonej_thickness
-    data1,data1header1 = nrrd.read(ROIDir+f"Segmentation-grayscale-Print-{NAME}.nrrd")
+    data1,data1header1 = nrrd.read(ROIDir+f"{NAME}.nrrd")
     ### save data1 to temporaryDirectory
     header = {'units': ['um', 'um', 'um'],'spacings': [51.29980,51.29980,51.29980]}
     nrrd.write(data1_nrrd,data1,header)
 
 
-# TODO: run your BoneJ thickness wrapper
-# table is the boneJ table, thickness_tif is a numpy array containing thickness image
     macro_file = "/gpfs_projects/sriharsha.marupudi/Trabecular_Spacing_API.py"
 
     fiji_path = "~/Fiji.app/ImageJ-linux64" #home directory
-#Run BoneJ in headless mode in commandline with arguments  
 
     fiji_cmd = "".join([fiji_path, " --ij2", " --headless", " --run", " "+macro_file, 
                      " \'image="+"\""+data1_nrrd+"\"", ", spacing_tif="+"\""+spacing_tif+"\"",
@@ -62,4 +55,3 @@ for txt in ROINRRD:
                      ", table_csv="+"\""+table_csv+"\""+"\'"])
 
     b = subprocess.call(fiji_cmd, shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
-    print(table_csv)
