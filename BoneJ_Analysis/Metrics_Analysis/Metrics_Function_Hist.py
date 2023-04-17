939670b4d27cf55b376efef6ed378864134e4a60
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-04-17 12:01:19 -0400

    Update Metrics_Function_Hist.py

--- BoneJ_Analysis/Metrics_Analysis/Metrics_Function_Hist.py
+++ BoneJ_Analysis/Metrics_Analysis/Metrics_Function_Hist.py
@@ -15,81 +15,57 @@ import statistics as stats
 
 
 def Thickness():
-    out_dir1 = "/gpfs_projects/sriharsha.marupudi/Thickness_Measurements_Print"
-    os.makedirs(out_dir1, exist_ok=True)
-     
-    data2=pd.read_csv("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print/Thickness_results.csv",usecols=['rowname','img.nrrd'])
-    data2.plot(kind='hist',bins=12)
+    data=pd.read_csv("/..csv)
+    data.plot(kind='hist',bins=12)
     plt.title("Thickness")
     plt.xlabel("Thickness (microns)")
-    # plt.ylim([0,20])
     plt.ylabel("Number of ROIs")
     plt.show()
-    plt.savefig("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print_Thickness_Histogram.png")
 
-def Spacing():
-    out_dir2 = "/gpfs_projects/sriharsha.marupudi/Spacing_Measurements_Print"
-    os.makedirs(out_dir2, exist_ok=True)      
-    data2=pd.read_csv("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print/Spacing_results.csv",usecols=['rowname','img.nrrd'])
-    data2.plot(kind='hist',bins=12)
+def Spacing(): 
+    data=pd.read_csv("/..csv)
+    data.plot(kind='hist',bins=12)
     plt.title("Spacing")
     plt.xlabel("Spacing (microns)")
     # plt.ylim([0,20])
     plt.ylabel("Number of ROIs")
     plt.show()
-    plt.savefig("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print_Spacing_Histogram.png")
     
-def BVTV(): 
-    
-     
-    data2=pd.read_csv("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print/Area_VolumeFraction_results.csv",usecols=['rowname','img.nrrd'])
-    data2.plot(kind='hist',bins=12)
+def BVTV():     
+    data=pd.read_csv("/..csv")
+    data.plot(kind='hist',bins=12)
     plt.title("BV/TV")
     plt.xlabel("BV/TV")
     # plt.ylim([0,20])
     plt.ylabel("Number of ROIs")
     plt.show()
-    plt.savefig("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print_Area_VolumeFraction_Histogram.png")
     
-def Anisotropy():
-    out_dir1 = "/gpfs_projects/sriharsha.marupudi/Anisotropy_Measurements_Print"
-    os.makedirs(out_dir1, exist_ok=True)
-          
-    data2=pd.read_csv("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print/Anisotropy_results.csv",usecols=['rowname','img.nrrd'])
-    data2.plot(kind='hist',bins=12)
+def Anisotropy():   
+    data=pd.read_csv("/..csv")
+    data.plot(kind='hist',bins=12)
     plt.title("Anisotropy")
     plt.xlabel("DA")
     # plt.ylim([0,20])
     plt.ylabel("Number of ROIs")
     plt.show()
-    plt.savefig("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print_Anisotropy_Histogram.png")
     
 def Connectivity():
-    out_dir1 = "/gpfs_projects/sriharsha.marupudi/Connectivity_Measurements_Print"
-    os.makedirs(out_dir1, exist_ok=True)
-            
-    data2=pd.read_csv("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print/Connectivity_results.csv",usecols=['rowname','img.nrrd'])
-    data=pd.read_csv("/gpfs_projects/sriharsha.marupudi/BoneJ_Metrics_Printed_Volumes.csv",usecols=['Thickness'])
+         
+    data=pd.read_csv("/..csv")
+    data.plot(kind='hist',bins=12)
     data2.plot(kind='hist',bins=12)
     plt.title("Connectivity")
     plt.xlabel("Connectivity Density ($1/microns^3$)")
-    # plt.ylim([0,20])
     plt.ylabel("Number of ROIs")
     plt.show()
-    plt.savefig("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print_Connectivity_Histogram.png")
     
 def EF():
-    out_dir1 = "/gpfs_projects/sriharsha.marupudi/Ellipsoid_Factor_Measurements_Print"
-    os.makedirs(out_dir1, exist_ok=True)     
-    data2=pd.read_csv("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print/Ellipsoid_Factor_results.csv",usecols=['rowname','img.nrrd'])
-
-    data2.plot(kind='hist',bins=12)
+    data=pd.read_csv("/..csv")
+    data.plot(kind='hist',bins=12) 
     plt.title("Ellipsoid Factor")
     plt.xlabel("Ellipsoid Factor")
-    # plt.ylim([0,20])
     plt.ylabel("Number of ROIs")
     plt.show()
-    plt.savefig("/gpfs_projects/sriharsha.marupudi/BoneJ_Results_Print_EF_Histogram.png")   
     
     
 Thickness()
