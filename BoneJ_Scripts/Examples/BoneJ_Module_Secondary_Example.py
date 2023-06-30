d2d0b85439408bfd5622fc5604ebb0683e89f8a5
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-06-30 15:31:07 -0400

    Update BoneJ_Module_Secondary_Example.py

--- BoneJ_Scripts/Examples/BoneJ_Module_Secondary_Example.py
+++ BoneJ_Scripts/Examples/BoneJ_Module_Secondary_Example.py
@@ -17,11 +17,16 @@ from BoneJ_Module_Secondary import Analyze_Skeleton
 from BoneJ_Module_Secondary import Intertrabecular_Angles 
 from BoneJ_Module_Secondary import Skeletonise 
 
+NAME = "emu"
+array,array1header = nrrd.read(f"/BoneJ_Headless-main/ROIs/{NAME}.nrrd")
+voxel_size = [51.29980, 51.29980, 51.29980] #microns 
+fiji_path = "~/Fiji.app/ImageJ-linux64"
+
  if __name__ == "__main__":   
- Fractal_Dimension_Results=Fractal_Dimension(array,voxel_size,fiji_path,startBoxSize=48,smallestBoxSize=6,scaleFactor=1.2,autoParam=False)
- Surface_Area_Result=Surface_Area(array,voxel_size,fiji_path)
- Analzye_Skeleton_Result=Analyze_Skeleton(array,voxel_size,fiji_path,pruneCycleMethod=None,pruneEnds=True,excludeRoi=False,calculateShortestPaths=True,verbose=True,displaySkeletons=True)
- Intertrabecular_Angles_Result=Intertrabecular_Angles(array,voxel_size,fiji_path,minimumValence=3,maximumValence=50,marginCutOff=10,minimumTrabecularLength=0,iteratePruning=False,
- printCentroids=False,useClusters=False,printCulledEdgePercentages=False)
- Skeletonise_Result=Skeletonise(array,voxel_size,fiji_path)
+  Fractal_Dimension_Results=Fractal_Dimension(array,voxel_size,fiji_path,startBoxSize=48,smallestBoxSize=6,scaleFactor=1.2,autoParam=False)
+  Surface_Area_Result=Surface_Area(array,voxel_size,fiji_path)
+  Analzye_Skeleton_Result=Analyze_Skeleton(array,voxel_size,fiji_path,pruneCycleMethod=None,pruneEnds=True,excludeRoi=False,calculateShortestPaths=True,verbose=True,displaySkeletons=True)
+  Intertrabecular_Angles_Result=Intertrabecular_Angles(array,voxel_size,fiji_path,minimumValence=3,maximumValence=50,marginCutOff=10,minimumTrabecularLength=0,iteratePruning=False,
+  printCentroids=False,useClusters=False,printCulledEdgePercentages=False)
+  Skeletonise_Result=Skeletonise(array,voxel_size,fiji_path)
 
