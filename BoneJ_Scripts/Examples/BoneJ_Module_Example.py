63287ecbd59e0118d1607df966d45c43622c38a6
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-07-06 00:14:49 -0400

    Add files via upload

--- BoneJ_Scripts/Examples/BoneJ_Module_Example.py
+++ BoneJ_Scripts/Examples/BoneJ_Module_Example.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import numpy as np
 import nrrd
 import os
@@ -9,6 +11,7 @@ import tifffile as tiff
 from contextlib import contextmanager
 import sys, os
 
+sys.path.append('/gpfs_projects/sriharsha.marupudi/BoneJ_Headless-main/BoneJ_Scripts/')
 from BoneJ_Module import Thickness
 from BoneJ_Module import Spacing
 from BoneJ_Module import Area_VolumeFraction
@@ -16,29 +19,22 @@ from BoneJ_Module import Connectivity
 from BoneJ_Module import Anisotropy
 from BoneJ_Module import Ellipsoid_Factor
 
-filepath = "/.../ROIs/Shrew.nrrd"
-array,array1header = nrrd.read(filepath)
+filepath = "/gpfs_projects/sriharsha.marupudi/BoneJ_Headless-main/ROIs/emu.nrrd"
+array,array1header = nrrd.read(filepath) 
 voxel_size = [51.29980, 51.29980, 51.29980] #microns 
 fiji_path = "~/Fiji.app/ImageJ-linux64"
 
 if __name__ == "__main__":
-     Thickness_result = Thickness(array,voxel_size,fiji_path,showMaps = True, maskArtefacts = True)
-     Spacing_result = Spacing(array,voxel_size,fiji_path,showMaps = True, maskArtefacts = True)
-     Area_VolumeFraction_result = Area_VolumeFraction(array,voxel_size,fiji_path)
-     Connectivity_result = Connectivity(array,voxel_size,fiji_path)
-     Anisotropy_result = Anisotropy(array,voxel_size,fiji_path,NDirs = 2000, nLines = 10000, samplingincrement = 1.73,
-     radii = False, eigens = False)
-     Ellipsoid_Factor(array, voxel_size, fiji_path,nVectors = 100,vectorIncrement =.435,skipRatio =1,contactSensitivity = 1
-     ,maxIterations = 100,maxDrift = .4,runs = 1,seedOnDistanceRidge = True,distanceThreshold = .6,seedOnTopologyPreserving = True
-     ,showFlinnPlots = True,showConvergence = True,showSecondaryImages = True) 
-
-
-   
+    Thickness_result = Thickness(array,voxel_size,fiji_path,showMaps = True, maskArtefacts = True)
+    Spacing_result = Spacing(array,voxel_size,fiji_path,showMaps = True, maskArtefacts = True)
+    Area_VolumeFraction_result = Area_VolumeFraction(array,voxel_size,fiji_path)
+    Connectivity_result = Connectivity(array,voxel_size,fiji_path)
+    Anisotropy_result = Anisotropy(array,voxel_size,fiji_path,NDirs = 2000, nLines = 10000, samplingincrement = 1.73,
+    radii = False, eigens = False)
+    Ellipsoid_Factor(array, voxel_size, fiji_path,nVectors = 100,vectorIncrement =.435,skipRatio =1,contactSensitivity = 1
+    ,maxIterations = 100,maxDrift = .4,runs = 1,seedOnDistanceRidge = True,distanceThreshold = .6,seedOnTopologyPreserving = True
+    ,showFlinnPlots = True,showConvergence = True,showSecondaryImages = True) 
     
     
-    
-    
-
-
-    
-   
+        
+       
