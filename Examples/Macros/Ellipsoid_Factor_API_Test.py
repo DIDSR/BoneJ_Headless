7e85c25b101e0d0ac31c2b9ce66c8e81a0ee3711
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2023-08-28 16:30:37 -0400

    Update Ellipsoid_Factor_API_Test.py
    Converted the tif images from ImgPlus to ImagePlus to save with IJ.save. All resultant tif images from Ellipsoid Factor can be saved now.

--- Examples/Macros/Ellipsoid_Factor_API_Test.py
+++ Examples/Macros/Ellipsoid_Factor_API_Test.py
@@ -1,37 +1,3 @@
-#@CommandService cs
-#@ DatasetIOService io
-#@ UIService ui
-#@Context ctxt
-
-
-#@ String image
-#@ String table_csv
-#@ String img_ef_tif
-#@ String img_volume_tif
-#@ String img_id_tif
-#@ String img_a_tif
-#@ String img_b_tif
-#@ String img_c_tif
-#@ String img_ab_tif
-#@ String img_bc_tif
-#@ String img_seed_points_tif
-#@ String img_flinn_peak_plot_tif
-#@ String img_unweighted_flinn_plot_tif
-#@ String nVectors 
-#@ String vectorIncrement
-#@ String skipRatio
-#@ String contactSensitivity
-#@ String maxIterations
-#@ String maxDrift
-#@ String runs
-#@ String distanceThreshold
-#@ String seedOnDistanceRidge
-#@ String seedOnTopologyPreserving
-#@ String showFlinnPlots 
-#@ String showConvergence 
-#@ String showSecondaryImages
-#@ String NAME
-#@ String outputdir
 
 from ij import IJ
 from net.imglib2.img import ImagePlusAdapter
@@ -48,24 +14,57 @@ from org.bonej.utilities import SharedTable
 from ij import ImagePlus
 import net.imagej.ImgPlus;
 from ij.io import FileSaver
-#Open image from path 
-outputdir 
+from net.imglib2.img.display.imagej import ImageJFunctions
+ 
+outputdir
 IJ.run("Clear BoneJ results");
-#open input image as I2-style image to be compatabile with wrapper
 input_image = IJ.openImage(image)
 output = ImagePlusAdapter.wrapImgPlus(input_image) 
-#"inputImage",output,"nVectors",100,"vectorIncrement",3,"skipRatio",1,"contactSensitivity",1,"maxIterations",50,"maxDrift",1.0,"runs",1,"seedOnDistanceRidge",True,"distanceThreshold",0.8,"seedOnTopologyPreserving",True,"showFlinnPlots",True,"showConvergence",True,"showSecondaryImages",True,"])
-#Run Ellipsoid Factor Plugin specifying parameters 
+ 
+ 
 wrapper = cs.run("org.bonej.wrapperPlugins.EllipsoidFactorWrapper", False, ["inputImage",output,"nVectors",nVectors,"vectorIncrement",vectorIncrement,"skipRatio",skipRatio,"contactSensitivity",contactSensitivity,"maxIterations",maxIterations,"maxDrift",maxDrift,"runs",runs,"seedOnDistanceRidge",seedOnDistanceRidge,"distanceThreshold",distanceThreshold,"seedOnTopologyPreserving",seedOnTopologyPreserving,"showFlinnPlots",showFlinnPlots,"showConvergence",showConvergence,"showSecondaryImages",showSecondaryImages])
 wrapperInstance = wrapper.get()
 outputs = wrapperInstance.getOutput("ellipsoidFactorOutputImages")
 
 EF = outputs.get(0)
+d = DefaultDataset(ctxt,EF)
+d = ImageJFunctions.wrap(d, "img_ef")
+IJ.save(d, outputdir +"img_ef.tif")
 
 
+V = outputs.get(1)
+v = DefaultDataset(ctxt,V)
+v = ImageJFunctions.wrap(v, "img_volume")
+IJ.save(v, outputdir+"img_volume.tif")
+
+ 
+AB = outputs.get(6)
+AB = DefaultDataset(ctxt,AB)
+AB = ImageJFunctions.wrap(AB, "img_ab")
+IJ.save(AB, outputdir+"img_ab.tif")
+
+BC = outputs.get(7)
+BC = DefaultDataset(ctxt,BC)
+BC = ImageJFunctions.wrap(BC, "img_bc")
+IJ.save(BC, outputdir+"img_bc.tif")
+
+
+B = outputs.get(4)
+B = DefaultDataset(ctxt,B)
+B = ImageJFunctions.wrap(B, "img_b")
+IJ.save(d, outputdir+"img_b.tif")
+
+C = outputs.get(5)
+C = DefaultDataset(ctxt,C)
+C = ImageJFunctions.wrap(C, "img_c")
+IJ.save(C, outputdir+"img_c.tif")
+
+FlinnPlots = outputs.get(9)
+FlinnPlots = DefaultDataset(ctxt,FlinnPlots)
+FlinnPlots = ImageJFunctions.wrap(FlinnPlots, "img_flinn_peak_plot")
+IJ.save(FlinnPlots, outputdir+"img_flinn_peak_plot.tif")
 
 table =SharedTable.getTable()
-
 
 f = open(outputdir+"table.csv", 'wb')
 f.write('\n')
@@ -83,46 +82,4 @@ for i in range(0,len(table)):
 	f.write('\n')
 f.close()
 			
-# print(table)
-
-
-
-d = DefaultDataset(ctxt,EF)
-io.save(d, outputdir+"/"+"img_ef.tif")
-
-# V = outputs.get(1)
-
-# d = DefaultDataset(ctxt,V)
-# io.save(d, outputdir+"/"+"img_volume.tif")
-
-# #ID = outputs.get(2)
-# #d = DefaultDataset(ctxt,ID)
-# #io.save(d, outputdir+"/"+"img_id2.tif")
-
-# AB = outputs.get(6)
-# d = DefaultDataset(ctxt,AB)
-# io.save(d, outputdir+"/"+"img_ab.tif")
-
-# BC = outputs.get(7)
-# d = DefaultDataset(ctxt,BC)
-# io.save(d, outputdir+"/"+"img_bc.tif")
-
-# #A = outputs.get(3)
-# #d = DefaultDataset(ctxt,A)
-# #io.save(d, outputdir+"/"+"img_a1.tif")
-
-# B = outputs.get(4)
-# d = DefaultDataset(ctxt,B)
-# io.save(d, outputdir+"/"+"img_b.tif")
-
-# C = outputs.get(5)
-# d = DefaultDataset(ctxt,C)
-# io.save(d, outputdir+"/"+"img_c.tif")
-
-# FlinnPlots = outputs.get(9)
-# d = DefaultDataset(ctxt,FlinnPlots)
-# io.save(d, outputdir+"/"+"img_flinn_peak_plot.tif")
-
-
-
-	
+print(table)
